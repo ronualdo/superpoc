eb1dedd7d5cebcc4f3ded39ef73781c6ea5d5298
ronualdo <raxmac@gmail.com>
2013-09-08 14:55:45 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superpoc::Application.config.secret_token = '05917bdf9f6f6fb87befa51866746f603ab79b4f59bb3bea6308c9a4d3be023f2d7a81c1f86435b74e5470db34cc050290bd305835c5838bdbd00f6dc269534c'
